8b9179e910d4a0bee81cef73a346dc1ab6ba2ac9
Kirk Shoop <kirkshoop@fb.com>
2020-03-03 12:56:48 -0800

    adding pipe read/write to epoll

--- examples/linux/io_epoll_test.cpp
+++ examples/linux/io_epoll_test.cpp
@@ -27,6 +27,9 @@
 #include <unifex/sync_wait.hpp>
 #include <unifex/transform.hpp>
 #include <unifex/when_all.hpp>
+#include <unifex/repeat.hpp>
+#include <unifex/typed_via.hpp>
+#include <unifex/with_query_value.hpp>
 
 #include <chrono>
 #include <cstdio>
@@ -42,6 +45,18 @@ template <typename F>
 auto lazy(F&& f) {
   return transform(just(), (F &&) f);
 }
+template <typename S>
+auto discard(S&& s) {
+  return transform(s, [](auto&&...){});
+}
+
+//! Seconds to warmup the benchmark
+static constexpr int WARMUP_DURATION = 3;
+
+//! Seconds to run the benchmark
+static constexpr int BENCHMARK_DURATION = 10;
+
+static constexpr unsigned char data[6] = {'h', 'e', 'l', 'l', 'o', '\n'};
 
 int main() {
   io_epoll_context ctx;
@@ -84,6 +99,87 @@ int main() {
   } catch (const std::exception& ex) {
     std::printf("error: %s\n", ex.what());
   }
+
+  auto [rPipe, wPipe] = open_pipe(scheduler);
+
+  inplace_stop_source stopWrite;
+  std::thread w{[&] { 
+      const auto buffer = as_bytes(span{data});
+      try {
+        while (!stopWrite.stop_requested()) {
+          sync_wait(
+            async_write_some(wPipe, buffer), stopWrite.get_token());
+        }
+      } catch (const std::error_code& ec) {
+        std::printf("async_write_some error: %s\n", ec.message().c_str());
+      } catch (const std::exception& ex) {
+        std::printf("async_write_some exception: %s\n", ex.what());
+      }
+    }};
+  scope_guard waitForWrites = [&]() noexcept {
+    stopWrite.request_stop();
+    w.join();
+  };
+
+  inplace_stop_source stopWarmup;
+  inplace_stop_source stopRead;
+  auto buffer = std::vector<char>{};
+  buffer.resize(1);
+  auto offset = 0;
+  auto reps = 0;
+  auto pipe_bench = [&](int seconds, auto& stopSource) {
+    return with_query_value(//just(), get_stop_token, stopSource.get_token());
+      discard(
+        when_all(
+          transform(
+            schedule_at(scheduler, now(scheduler) + std::chrono::seconds(seconds)), [&]{
+              stopSource.request_stop();
+            }),
+          typed_via(
+            repeat(
+              transform(
+                discard(
+                  async_read_some(rPipe, as_writable_bytes(span{buffer.data() + 0, 1}))),
+                [&]{
+                  assert(data[(reps + offset)%sizeof(data)] == buffer[0]);
+                  ++reps;
+                })), 
+            scheduler)
+        )), get_stop_token, stopSource.get_token());
+  };
+  auto start = std::chrono::steady_clock::now();
+  auto end = std::chrono::steady_clock::now();
+  try {
+    sync_wait(
+      sequence(
+        pipe_bench(WARMUP_DURATION, stopWarmup), // warmup
+        lazy([&]{
+          // restart reps and keep offset in data
+          offset = reps%sizeof(data);
+          reps = 0;
+          // exclude the warmup time
+          start = std::chrono::steady_clock::now();
+          printf("warmup completed!\n");
+        }),
+        pipe_bench(BENCHMARK_DURATION, stopRead)), 
+      stopRead.get_token());
+    int ms = (int)std::chrono::duration_cast<std::chrono::milliseconds>(
+            end - start)
+            .count();
+    int ns = (int)std::chrono::duration_cast<std::chrono::nanoseconds>(
+            end - start)
+            .count();
+    std::printf(
+        "completed in %i ms, %ins-per-op, %iops-per-ms\n",
+        ms,
+        ns/reps,
+        reps/ms);
+  } catch (const std::error_code& ec) {
+    std::printf("async_read_some error: %s\n", ec.message().c_str());
+  } catch (const std::exception& ex) {
+    std::printf("async_read_some exception: %s\n", ex.what());
+  }
+
   return 0;
 }
 
